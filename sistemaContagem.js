9312135a6f28eb083a595a89ebddbbd86465deb2
Carolina Cavallaro <carol.cavallaro@gmail.com>
2022-08-19 16:14:18 -0300

    Adicionado listaAlunos à classificação

--- sistemaContagem.js
+++ sistemaContagem.js
@@ -2,14 +2,14 @@
 // Número ímpar: escreva "ímpar" e o número correspondente.
 // Número zero: escreva "zero" e o número correspondente.
 
-let listaAlunos = 10;
+let listaAlunos = ["Carolina", "Diego", "Velma", "Pingado", "Jujuba", "Farofa", "Gemada", "Cocada", "Paçoca", "Pitaia", "Biscoito", "Pipoca"];
 
-for (contador = 0;  contador <= listaAlunos; contador++) {
+for (contador = 0;  contador <= listaAlunos.length; contador++) {
     if (contador == 0) {
-        console.log(contador + " zero");
+        console.log(contador + " zero - " + listaAlunos[0]);
     } else if (contador % 2) {
-        console.log(contador + " ímpar");
+        console.log(contador + " ímpar - " + listaAlunos[contador]);
     } else {
-        console.log (contador + " par");
+        console.log (contador + " par - " + listaAlunos[contador]);
     }
 }
